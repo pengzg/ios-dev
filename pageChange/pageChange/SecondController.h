0e5f0e8a2af046baba7d5976237bcef599000294
彭宗阁 <pengzongge@pengzonggedeMacBook-Pro.local>
2018-09-23 09:31:23 +0800

    pagechange

---
+++ pageChange/pageChange/SecondController.h
@@ -0,0 +1,17 @@
+//
+//  SecondController.h
+//  pageChange
+//
+//  Created by 彭宗阁 on 2018/9/22.
+//  Copyright © 2018 pengzg. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SecondController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ pageChange/pageChange/SecondController.m
@@ -0,0 +1,13 @@
+//
+//  SecondController.m
+//  pageChange
+//
+//  Created by 彭宗阁 on 2018/9/22.
+//  Copyright © 2018 pengzg. All rights reserved.
+//
+
+#import "SecondController.h"
+
+@implementation SecondController
+
+@end
