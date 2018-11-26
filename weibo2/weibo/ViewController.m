68e2062ee20a9a721e47bf819f13f935e461d711
彭宗阁 <pengzongge@pengzonggedeMacBook-Pro.local>
2018-11-26 21:51:10 +0800

    map

--- weibo2/weibo/ViewController.m
+++ weibo2/weibo/ViewController.m
@@ -27,8 +27,8 @@
     NSString *content = @"iOS是由苹果公司开发的移动操作系统 [1]  。苹果公司最早于2007年1月9日的Macworld大会上公布这个系统，最初是设计给iPhone使用的，后来陆续套用到iPod touch、iPad以及Apple TV等产品上。iOS与苹果的Mac OS X操作系统一样，属于类Unix的商业操作系统。原本这个系统名为iPhone OS，因为iPad，iPhone，iPod touch都使用iPhone OS，所以2010WWDC大会上宣布改名为iOS（iOS为美国Cisco公司网络设备操作系统注册商标，苹果改名已获得Cisco公司授权）。";
     Weibo *weibo1 = [Weibo weiboWithIcon:@"01.jpg" name:@"彭" time:@"2018-02-02 23:32:33" source:@"来自iphone客户端" content:content img:@"03.jpg" vip:nil];
     Weibo *weibo2 = [Weibo weiboWithIcon:@"02.jpg" name:@"彭宗" time:@"2018-02-02 23:32:33" source:@"来自iphone客户端" content:@"这是测试" img:@"02.jpg" vip:@"02.jpg"];
-    Weibo *weibo3 = [Weibo weiboWithIcon:@"03.jpg" name:@"彭宗阁" time:@"2018-02-02 23:32:33" source:@"来自iphone客户端" content:@"这是测试" img:@"01.jpg" vip:@"02.jpg"];
-    Weibo *weibo4 = [Weibo weiboWithIcon:@"02.jpg" name:@"彭宗阁彭" time:@"2018-02-02 23:32:33" source:@"来自iphone客户端" content:@"这是测试" img:@"02.jpg" vip:@"02.jpg"];
+    Weibo *weibo3 = [Weibo weiboWithIcon:@"03.jpg" name:@"彭宗阁" time:@"2018-02-02 23:32:33" source:@"来自iphone客户端" content:@"这是测试sfdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd" img:@"01.jpg" vip:@"02.jpg"];
+    Weibo *weibo4 = [Weibo weiboWithIcon:@"02.jpg" name:@"彭宗阁彭" time:@"2018-02-02 23:32:33" source:@"来自iphone客户端" content:@"这是测试" img:@"http://imgtest.sqkx.net/static/upload/image/100001/common/2018/11/21/1542779170660053866.jpg" vip:@"02.jpg"];
     
     [_weiboList addObjectsFromArray:@[weibo1,weibo2, weibo3, weibo4]];
     
@@ -63,6 +63,8 @@
     //NSLog(@"height==>%ld", f.cellHeight);
     return f.cellHeight;
 }
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
