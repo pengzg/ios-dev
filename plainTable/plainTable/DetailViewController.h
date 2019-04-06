5ff7990d7b645216fba7ecdcd4ff88b5bcb94e25
彭宗阁 <pzgmissyou@gmail.com>
2019-04-06 16:00:22 +0800

    edit

---
+++ plainTable/plainTable/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  plainTable
+//
+//  Created by 彭宗阁 on 2019/4/6.
+//  Copyright © 2019 pengzg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DetailViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ plainTable/plainTable/DetailViewController.m
@@ -0,0 +1,32 @@
+//
+//  DetailViewController.m
+//  plainTable
+//
+//  Created by 彭宗阁 on 2019/4/6.
+//  Copyright © 2019 pengzg. All rights reserved.
+//
+
+#import "DetailViewController.h"
+
+@interface DetailViewController ()
+
+@end
+
+@implementation DetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
